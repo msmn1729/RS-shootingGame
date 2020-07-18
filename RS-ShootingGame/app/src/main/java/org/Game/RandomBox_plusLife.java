8d8ef0de249013aa8e97ef05cdc0228d85744ef5
KIm yeong hyun <gus7518@naver.com>
2020-07-18 21:09:39 +0900

    2020 7 18 21:09 add RandomBox

---
+++ RS-ShootingGame/app/src/main/java/org/Game/RandomBox_plusLife.java
@@ -0,0 +1,23 @@
+package org.Game;
+
+import android.graphics.Bitmap;
+
+import com.example.gameframework.AppManager;
+import com.example.gameframework.R;
+
+import org.Game.RandomBox;
+
+public class RandomBox_plusLife extends RandomBox {
+    public RandomBox_plusLife() {
+        super(AppManager.getInstance().getBitmap(R.drawable.randombox));
+        this.initSpriteData(100,100,1,1);
+        speed = 10;
+        boxtype = 4;
+    }
+
+    @Override
+    public void Update(long gameTime) {
+        super.Update(gameTime);
+        m_BoundBox.set(m_x,m_y,m_x+120,m_y+120);
+    }
+}

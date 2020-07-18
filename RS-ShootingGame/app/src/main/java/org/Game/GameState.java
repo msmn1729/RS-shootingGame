ffeaf5eab53e78b0d6d0cc298063db8e74c5b530
KIm yeong hyun <gus7518@naver.com>
2020-07-18 21:12:39 +0900

    2020 7 18 21:09 add RandomBox

--- RS-ShootingGame/app/src/main/java/org/Game/GameState.java
+++ RS-ShootingGame/app/src/main/java/org/Game/GameState.java
@@ -335,6 +335,7 @@ public class GameState implements IState {
 //                    AppManeger.getInstance().getGameView().viberator();
 //                }
                 return;
+                
             }
         }
     }
